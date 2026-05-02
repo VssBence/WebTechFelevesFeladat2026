256231a75e4756760b491e3769609f268e127585
Bence Vass <bence.vass@student.uni-miskolc.hu>
2026-05-02 19:11:08 +0200

    Hibakezelés javítása
    A lemaradt returnöket pótoltam a hibakezelésnél
    Illetve a kocsilista változót beraktam inkább az app.js-be

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const NeptunCode = "Z5BZ1O";
 
 
+const kocsilista = ['Toyota', 'Honda', 'Ford', 'Chevrolet', 'Nissan', 'BMW', 'Mercedes-Benz', 'Volkswagen', 'Audi', 'Hyundai', 'Kia', 'Subaru', 'Lexus', 'Mazda', 'Tesla', 'Jeep', 'Porsche', 'Volvo', 'Jaguar', 'Land Rover', 'Mitsubishi', 'Ferrari', 'Lamborghini'];
+
+
 function loadNavigation(){
     fetch('./navbar.html')
         .then(res => res.text())
